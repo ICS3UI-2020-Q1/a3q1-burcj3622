1a1ebf119db36eede16566a273969be928803adc
burcj3622 <burcj3622@wrdsb.ca>
2020-10-05 18:51:22 +0000

    number

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -10,6 +11,22 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
+    Scanner input = new Scanner( System.in);
     
+  System.out.println("Please enter three integers on separate lines");
+
+    int num1 = input.nextInt();
+    int num2 = input.nextInt();
+    int num3 = input.nextInt();
+
+    if( num1 >= num2 && num1 >= num3)
+     System.out.println(num1+" is the largest Number");
+
+    else if (num2 >= num1 && num2 >= num3)
+     System.out.println(num2+" is the largest Number");
+
+    else
+      System.out.println(num3+" is the largest Number");
   }
 }
+
